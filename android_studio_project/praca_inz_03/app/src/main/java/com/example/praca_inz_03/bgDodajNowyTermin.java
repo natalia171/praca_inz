4bf4b39f620e961b9bf33ad99e8fa1d52da7ca55
Natalia Wojdat <natalia.wojdat15@gmail.com>
2021-03-21 19:07:43 +0100

    merge brancha dodaj wizyte

--- android_studio_project/praca_inz_03/app/src/main/java/com/example/praca_inz_03/bgDodajNowyTermin.java
+++ android_studio_project/praca_inz_03/app/src/main/java/com/example/praca_inz_03/bgDodajNowyTermin.java
@@ -21,6 +21,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class bgDodajNowyTermin extends AsyncTask <String, Void,String> {
 
@@ -70,29 +72,54 @@ public class bgDodajNowyTermin extends AsyncTask <String, Void,String> {
             http.setDoInput(true);
             http.setDoOutput(true);
 
-            OutputStream ops = http.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops,"UTF-8"));
-            String data = URLEncoder.encode("CZAS_START","UTF-8")+"="+URLEncoder.encode(poczatekWizyty,"UTF-8")
-                    +"&&"+ URLEncoder.encode("CZAS_STOP","UTF-8")+"="+URLEncoder.encode(koniecWizyty,"UTF-8")
-                    +"&&"+URLEncoder.encode("ID_LEKARZA","UTF-8")+"="+URLEncoder.encode(idLekarza,"UTF-8");
-            writer.write(data);
-            writer.flush(); // wysyła o co było napisane przez buffered writer
-            writer.close(); // zamyka buffered writer
-            ops.close(); // konczy tworzenie stringa do wyslania?????????
-
-
-            InputStream ips = http.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(ips,"ISO-8859-1"));
-            String line ="";
-            while ((line = reader.readLine()) != null)
-            {
-                result += line;
-                Log.d("bgPotwierzWizyte","Odpowiedz "+line);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date czas_od =null;
+            Date czas_do = null;
+            Date czas_od_mod =null;
+            Date czas_do_mod = null;
+            try {
+                czas_od = sdf.parse(poczatekWizyty);
+                czas_do = sdf.parse(koniecWizyty);
+            } catch (Exception ex) {
+                Log.v("Exception", ex.getLocalizedMessage());
             }
-            reader.close();
-            ips.close();
-            http.disconnect();
 
+            Log.d("15min", "Czas od "+poczatekWizyty+" sparsowana "+czas_od.toString());
+            int kwadrans=1000*60*15;
+            int iloscKwadransow = (int) ((czas_do.getTime() - czas_od.getTime()) /(kwadrans));
+            String s_data_od= null;
+            String s_data_do= null;
+
+            for (int i=0;i<iloscKwadransow;i++) {
+
+                czas_od_mod = new Date(czas_od.getTime() + i * kwadrans);
+                czas_do_mod = new Date(czas_od.getTime() + (i + 1) * kwadrans);
+                s_data_od = sdf.format(czas_od_mod);
+                s_data_do = sdf.format(czas_do_mod);
+
+                OutputStream ops = http.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops, "UTF-8"));
+
+                String data = URLEncoder.encode("CZAS_START", "UTF-8") + "=" + URLEncoder.encode(s_data_od, "UTF-8")
+                        + "&&" + URLEncoder.encode("CZAS_STOP", "UTF-8") + "=" + URLEncoder.encode(s_data_do, "UTF-8")
+                        + "&&" + URLEncoder.encode("ID_LEKARZA", "UTF-8") + "=" + URLEncoder.encode(idLekarza, "UTF-8");
+                writer.write(data);
+                writer.flush(); // wysyła o co było napisane przez buffered writer
+                writer.close(); // zamyka buffered writer
+                ops.close(); // konczy tworzenie stringa do wyslania?????????
+
+
+                InputStream ips = http.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(ips, "ISO-8859-1"));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    result += line;
+                    Log.d("bgPotwierzWizyte", "Odpowiedz " + line);
+                }
+                reader.close();
+                ips.close();
+                http.disconnect();
+            }
             //zle sformuowany adres url
         } catch (MalformedURLException e) {
             result = e.getMessage();
